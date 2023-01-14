9c065884c1001e858a095629131e7c364f1b99fa
gema <120004533+mrmongkeyy@users.noreply.github.com>
2023-01-14 16:00:01 +0700

    Update cika.js

--- private/engine/cika.js
+++ private/engine/cika.js
@@ -1,7 +1,4 @@
 const {Configuration,OpenAIApi} = require('openai');
-const readline = require('readline');
-readline.cursorTo(process.stdout,0,0);
-readline.clearScreenDown(process.stdout);
 const mrx = {
 	async start(config){
 		this.interface = config.interface;
